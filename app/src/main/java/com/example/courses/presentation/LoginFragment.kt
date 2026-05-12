062deed62f09802ff4ca93037f9ec1b2a0beebe1
ShetininaLiza <shetinina.l2000@gmail.com>
2026-05-12 10:33:03 +0400

    Добавление перехода от одного фрагмента к другому

---
+++ app/src/main/java/com/example/courses/presentation/LoginFragment.kt
@@ -0,0 +1,56 @@
+package com.example.courses.presentation
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.courses.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment(R.layout.fragment_login) {
+    /*
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+    */
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnNext = view.findViewById<Button>(R.id.btnNext)
+        btnNext.setOnClickListener {
+            //переход в другой фрагмент
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.frameLayout, CoursesFragment())
+                ?.commit()
+        }
+    }
+
+}
--- app/src/main/java/com/example/courses/presentation/MainActivity.kt
+++ app/src/main/java/com/example/courses/presentation/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.courses.presentation
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +18,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayout, LoginFragment())
+            .commit()
     }
 }
